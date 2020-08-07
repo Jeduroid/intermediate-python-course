930547365ba25257aaace5dc1324cc2c738234e2
Jeduroid <jeduroid@gmail.com>
2020-08-07 22:34:35 +0530

    Code for custom dice with custom sides

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,18 @@
 import random
 
 def main():
-  dice_rolls = 2
+  dice_rolls = int(input('\nHow many dice you want to roll : '))
+  dice_size = int(input('\nHow many sides a dice have : '))
   dice_sum = 0
   for i in range(0,dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_size)
     dice_sum += roll 
-    print(f'You rolled a {roll}')
+    if roll == 1:
+      print(f'You rolled a {roll} ! Critical Failure !')
+    elif roll ==dice_size:  
+      print(f'You rolled a {roll} ! Criticial Success !')
+    else:
+      print(f'You rolled a {roll}')
   print(f'Dice sum = {dice_sum}')
 
 if __name__== "__main__":
